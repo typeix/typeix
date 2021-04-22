4d9acad8c82b3233187fb978222e2f4c476284d9
igorzg <igor.ivanovic@pm.me>
2021-04-22 21:50:54 +0200

    Enable coveralls

--- coveralls-merge.js
+++ coveralls-merge.js
@@ -1,11 +1,23 @@
-const coveralls = require('@sourceallies/coveralls-merge');
+const fs = require("fs");
 // create reports
-const packages = ["di", "logger", "metadata", "modules", "resty", "router", "utils"];
-coveralls.sendReports(
-  packages.map(name => {
-    return {
-      type: "lcov",
-      reportFile: "./packages/" + name + "/coverage/lcov.info"
-    }
-  })
-);
+const packages = ["di", "logger", "metadata", "modules", "resty", "router", "utils"]
+  .map(name => "./packages/" + name + "/coverage/lcov.info")
+  .map(file => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(file,  (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      })
+    })
+  });
+
+
+exports.module = Promise.all(packages).then(data =>
+  new Promise(
+    resolve =>
+      fs.writeFile("./lcov.info", Buffer.concat(data), resolve))
+)
+;
